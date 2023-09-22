cfe067fa001a27ca559885e200cd656608051c54
Kellen Kern <kellenrocha201711@gmail.com>
2023-09-22 17:01:28 -0300

    feature: signIn

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,5 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+import { Input } from '../../components/input';
+import {
+  LoginContainer,
+  Logo,
+  CompanyName,
+  Link,
+  Button,
+} from './styled';
+import logo from '../../assets/logobranco.png'
+
 export const SignIn = () => {
+  const navigate = useNavigate()
+
+  const handleNavigate = () => {
+    navigate('/home')
+  }
   return (
-    <p>sign In</p>
-  )
+    <>
+      <LoginContainer>
+        <Logo src={logo} alt="Logo da Empresa" />
+        <CompanyName>COOL TEA COMPANY</CompanyName>
+        <Input type="email" placeholder="Email" />
+        <Input type="password" placeholder="Senha" />
+        <Button onClick={handleNavigate}>Entrar</Button>
+        <Link>Esqueci minha senha</Link>
+        <Link>Cadastrar</Link>
+      </LoginContainer>
+    </>
+  );
 }
+
