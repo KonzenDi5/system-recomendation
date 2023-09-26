202720c40c9d29e713b2faac784f9bf4fa0b50bf
Lucas Konzen <104283188+KonzenDi5@users.noreply.github.com>
2023-09-26 00:55:00 -0300

    deploy: localstorage

--- src/pages/Questions/index.js
+++ src/pages/Questions/index.js
@@ -1,73 +1,140 @@
-import { useState } from "react";
-import { Card, Content, DivContent, Progress, ProgressBar, Title } from "./style"
+import { useState, useEffect } from "react";
+import { Card, Content, DivContent, Progress, ProgressBar, Title } from "./style";
 import { Button } from "../../components/button";
 import { useNavigate } from "react-router-dom";
 
 export const Questions = () => {
-  const [currentQuestion, setCurrentQuestion] = useState(0)
-  const [answers, setAnswers] = useState([])
-  const [selectedAnswer, setSelectedAnswer] = useState(null)
-  const navigate = useNavigate()
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [answers, setAnswers] = useState([]);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const savedAnswers = localStorage.getItem("answers");
+    if (savedAnswers) {
+      setAnswers(JSON.parse(savedAnswers));
+    }
+  }, []);
+
+  const generateUniqueCode = (questionIndex, answerIndex) => {
+    return `P${questionIndex + 1}A${answerIndex + 1}${Math.random().toString(36).substr(2, 5)}`;
+  };
 
   const questions = [
     { 
-      question: "Como é sua alimentação?", 
+      question: "Qual seu nível de disposição? (bem estar físico)",
       answers: [
-        "Me sinto constantemente cansado e sem motivação. Na correria, a alimentação saudável fica em segundo plano.",
-        "Sinto uma leve fadiga, mas estou mais atento à minha saúde. Estou aprendendo a equilibrar minha dieta",
-        "Estou mais ativo, mas ainda caio na tentação do fast food/comfort food como pizzas e comida congelada. Busco equilíbrio.",
-        "Sinto-me em harmonia, com energia e foco na saúde. Minha alimentação é um reflexo do meu bem-estar"
-      ] 
+        "a) Sinto disposição para realizar todas as atividades do meu dia.",
+        "b) Sinto uma leve sensação de cansaço, mas sem muitos problemas para realizar minhas atividades.",
+        "c) Me sinto bastante cansado, pois realizo muitas atividades no meu dia.",
+        "d) Me sinto constantemente cansado e sem motivação para realizar minhas atividades."
+      ]
     },
-    { question: "Pergunta 2", answers: ["A", "B", "C", "D"] },
-    { question: "Pergunta 3", answers: ["A", "B", "C", "D"] },
-    { question: "Pergunta 4", answers: ["A", "B", "C", "D"] },
-    { question: "Pergunta 5", answers: ["A", "B", "C", "D"] },
-    { question: "Pergunta 6", answers: ["A", "B", "C", "D"] }, 
-  ]
+    { 
+      question: "Como é sua alimentação? (bem estar físico)",
+      answers: [
+        "a) Tenho cuidado para manter uma dieta saudável e equilibrada.",
+        "b) Possuo uma boa educação alimentar e raramente exagero.",
+        "c) Como bastante fast food e/ou doces durante a semana, mas tento me alimentar bem às vezes.",
+        "d) Na correria do dia a dia, acabo não conseguindo me alimentar de forma saudável."
+      ]
+    },
+    { 
+      question: "Qual seu nível de estresse atualmente? (bem estar emocional)",
+      answers: [
+        "a) Consigo encarar minha rotina de forma calma e controlada.",
+        "b) Possuo alguns poucos momentos de irritação durante a semana.",
+        "c) Passo grande parte do meu dia irritado/estressado, mas consigo separar algum momento do meu dia para relaxar.",
+        "d) Estou em constante pressão e estresse, sinto que estou prestes a explodir."
+      ]
+    },
+    { 
+      question: "Como estão seus níveis de foco e atenção? (bem estar mental)",
+      answers: [
+        "a) Consigo focar nas minhas atividades de forma plena e sem distrações.",
+        "b) Consigo realizar minhas tarefas diárias, mas me distraio em alguns momentos.",
+        "c) Tenho pensamentos repetitivos e não consigo focar no que preciso fazer.",
+        "d) Estou sempre preocupado(a) com o futuro e/ou triste com o passado."
+      ]
+    },
+    { 
+      question: "Como você se sente em seu círculo social? (bem estar social)",
+      answers: [
+        "a) Possuo uma ótima rede de apoio e faço amigos facilmente.",
+        "b) Tenho um bom ciclo de relações sociais.",
+        "c) Tenho dificuldades para fazer amizades.",
+        "d) Me sinto muito sozinho(a) e não sei como criar uma rede de apoio."
+      ]
+    },
+    { 
+      question: "Espiritualidade e Perspectiva de Vida (bem estar espiritual e intelectual)",
+      answers: [
+        "a) Sempre aprendo com momentos difíceis e estou em constante evolução.",
+        "b) Possuo facilidade em aceitar o momento presente e agradeço pelas minhas conquistas.",
+        "c) Tenho dificuldades em vivenciar situações desafiadoras e me sinto triste por estar desconfortável.",
+        "d) Sinto uma forte irritação quando as coisas estão fora do meu controle."
+      ]
+    },
+    // Adicione aqui as outras perguntas e respostas
+  ];
 
   const nextQuestion = () => {
     if (currentQuestion < questions.length - 1) {
-      setCurrentQuestion(currentQuestion + 1)
+      setCurrentQuestion(currentQuestion + 1);
     } else {
-      console.log(answers)
-      navigate('/trilha')
+      localStorage.setItem("answers", JSON.stringify(answers));
+      navigate("/trilha");
     }
+  };
+
+  const selectAnswer = (answer, answerIndex) => {
+    setSelectedAnswer({ answer, code: generateUniqueCode(currentQuestion, answerIndex) });
+  };
+
+  // Verificar se currentQuestion é um índice válido
+  if (currentQuestion >= questions.length || currentQuestion < 0) {
+    return <div>Erro: Índice de pergunta inválido.</div>;
   }
 
-  const selectAnswer = (answer) => {
-    setSelectedAnswer(answer);
-  }
-
-    return (
-      <DivContent>
-        <ProgressBar>
-          <Progress width={`${((currentQuestion) / (questions.length)) * 100}%`} />
-        </ProgressBar>
-        <Content>
-          <Title>{questions[currentQuestion].question}</Title>
-          {questions[currentQuestion].answers.map((answer, index) => (
-              <Card 
-              key={index} 
-              onClick={() => selectAnswer(answer)}
-              style={answer === selectedAnswer ? { backgroundColor: '#7A81DA'} : {}}
-          >
-              {answer}
-          </Card>
-          ))}
-        </Content>
-        <Button 
-          label={'Next Question'}
-          type={'submit'}
-          onClick={() => {
-            if (selectedAnswer) {
-              setAnswers(prevAnswers => [...prevAnswers, selectedAnswer]);
-              setSelectedAnswer(null);
-              nextQuestion();
+  return (
+    <DivContent>
+      <ProgressBar>
+        <Progress width={`${((currentQuestion) / (questions.length)) * 100}%`} />
+      </ProgressBar>
+      <Content>
+        <Title>{questions[currentQuestion].question}</Title>
+        {questions[currentQuestion].answers.map((answer, index) => (
+          <Card
+            key={index}
+            onClick={() => selectAnswer(answer, index)}
+            style={
+              selectedAnswer && selectedAnswer.answer === answer
+                ? { backgroundColor: "#7A81DA" }
+                : {}
             }
-          }}
-        />
-      </DivContent>
-    )
-  }
-  
+          >
+            {answer}
+          </Card>
+        ))}
+      </Content>
+      <Button
+        label={"Next Question"}
+        type={"submit"}
+        onClick={() => {
+          if (selectedAnswer) {
+            setAnswers((prevAnswers) => [
+              ...prevAnswers,
+              {
+                question: questions[currentQuestion].question,
+                answer: selectedAnswer.answer,
+                code: selectedAnswer.code,
+              },
+            ]);
+            setSelectedAnswer(null);
+            nextQuestion();
+          }
+        }}
+      />
+    </DivContent>
+  );
+};
